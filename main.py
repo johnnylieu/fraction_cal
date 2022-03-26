623a46288a087d6632ffedab39672ae1da71324e
Johnny <JohnnyLieu@yahoo.com>
2022-03-26 16:02:21 -0700

    removed commented code

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 # Write a function which takes a string input and returns whether the string is an operator or not
 
-# import sys
-
 def is_it_operator(item):
     is_operator = False
     if item == '+' or item == '-' or item == '*' or item == '/':
         is_operator = True
     print(f"{item} is an operator: {is_operator}")
-    # print(f"sys argvs: {sys.argv}") # works
-    # is_operator = False
-    # if sys.argv[3] == '+' or sys.argv[3] == '-' or sys.argv[3] == '*' or sys.argv[3] == '/':
-    #     is_operator = True
-    # print(is_operator)
 
 is_it_operator("+")
 is_it_operator("-")
