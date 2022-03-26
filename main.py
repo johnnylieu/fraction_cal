e72a984ed335b07ddfccd6d4ede2de2f0addae14
Johnny <JohnnyLieu@yahoo.com>
2022-03-26 16:23:54 -0700

    trying to get sum of list

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-# Write a function which takes a string input and returns whether the string is an operator or not
+# Coding Challenge
+ 
+# Write a command line program in the language of your choice that will take operations on fractions as an input and produce a fractional result.
+# Legal operators shall be *, /, +, - (multiply, divide, add, subtract)
+# Operands and operators shall be separated by one or more spaces
+# Mixed numbers will be represented by whole_numerator/denominator. e.g. "3_1/4"
+# Improper fractions and whole numbers are also allowed as operands 
+# Example run:
+# ? 1/2 * 3_3/4
+# = 1_7/8
+ 
+# ? 2_3/8 + 9/8
+# = 3_1/2
+
+# We recommend that you include test cases and unit tests. When you're done please email us a link to a GitHub repository, etc.
+# We'd also love if you could share an ETA when you get the chance. Look forward to seeing what you can come up with for us.
+
+# return integer first
+# look into % (modular) for fraction
+# remainder/denominator
+
+from fractions import *
+
+input = ["3.9", "+", "6.8", "-", "1.3"]
+print(f"input: {input}")
 
 def is_it_operator(item):
     is_operator = False
     if item == '+' or item == '-' or item == '*' or item == '/':
         is_operator = True
     print(f"{item} is an operator: {is_operator}")
+    filtered_input = [x for x in input if x!="+" if x!="-" if x!="*" if x!= "/"]
+    print(f"filtered list: {filtered_input}")
+    add = sum(filtered_input)
+    print(f"sum of input is: {add}")
 
-is_it_operator("+")
-is_it_operator("-")
-is_it_operator("*")
-is_it_operator("/")
-is_it_operator("5")
-is_it_operator(3)
-is_it_operator("T")
+is_it_operator(input)
 
     # print(f"sys argvs: {sys.argv}") # works
     # is_operator = False
