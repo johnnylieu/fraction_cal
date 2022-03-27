9dafff2374a2a4c27f75cd23d0633a23913b94f8
Johnny <JohnnyLieu@yahoo.com>
2022-03-27 11:42:36 -0700

    still getting errors

--- test_get_num1_and_get_num2.py
+++ test_get_num1_and_get_num2.py
@@ -6,7 +6,8 @@ from main import get_num1, get_num2, num1_whole_number, num2_whole_number
 
 class Test(unittest.TestCase):
     def test_get_num1(self):
-        self.assertAlmostEqual(get_num1(1), num1_whole_number = 1)
+        # if 1 is enter, num1_whole_number should be 1
+        self.assertEqual(get_num1(2), num1_whole_number = '2')
 
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+    unittest.main()
