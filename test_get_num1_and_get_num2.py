d48efd977a4438fe9df08b8e50c09fbac9864869
Johnny <JohnnyLieu@yahoo.com>
2022-03-27 11:40:00 -0700

    still trying to learn about unittest in python

--- test_get_num1_and_get_num2.py
+++ test_get_num1_and_get_num2.py
@@ -4,18 +4,9 @@ from fractions import *
 from pip import main
 from main import get_num1, get_num2, num1_whole_number, num2_whole_number
 
-def TestGetNum1(self):
-    # number should be imported to num1_whole_number so its length should be greater than 0
-    self.assertAlmostEqual(get_num1(1), (len(num1_whole_number) > 0))
+class Test(unittest.TestCase):
+    def test_get_num1(self):
+        self.assertAlmostEqual(get_num1(1), num1_whole_number = 1)
 
-def TestGetNum1(self):
-    # number should be imported to num1_whole_number so its length should be greater than 0
-    self.assertAlmostEqual(get_num1(3/4), (len(num1_whole_number) > 0))
-
-def TestGetNum2(self):
-    # number should be imported to num1_whole_number so its length should be greater than 0
-    self.assertAlmostEqual(get_num2(1), (len(num2_whole_number) > 0))
-
-def TestGetNum2(self):
-    # number should be imported to num1_whole_number so its length should be greater than 0
-    self.assertAlmostEqual(get_num2(2_1/2), (len(num2_whole_number) > 0))
+# if __name__ == "__main__":
+#     unittest.main()
