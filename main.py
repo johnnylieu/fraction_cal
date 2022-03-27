192addf96de71c2fb106014328f4c58975b1f4e8
Johnny <JohnnyLieu@yahoo.com>
2022-03-26 23:54:45 -0700

    commented out some test prints

--- main.py
+++ main.py
@@ -33,23 +33,23 @@ def get_num1_and_num2():
     # print(f"num2: {num2}")
 
     if sys.argv[3] == "+":
-        print(f"num1 and num2: {num1} & {num2}")
+        # print(f"num1 and num2: {num1} & {num2}")
         sum = num1 + num2
-        print(f"sum: {sum}")
+        # print(f"sum: {sum}")
         sum = str(sum)
         sum = sum.split("/")
-        print(f"sum: {sum}")
+        # print(f"sum: {sum}")
         numerator = sum[0]
         denominator = sum[1]
-        print(f"numerator & denominator: {numerator} & {denominator}")
+        # print(f"numerator & denominator: {numerator} & {denominator}")
         modular = int(numerator) % int(denominator)
-        print(f"modular: {modular}")
+        # print(f"modular: {modular}")
         if abs(int(numerator)) > abs(int(denominator)):
             whole = int(numerator) / int(denominator)
             whole = str(whole)
             whole = whole.split(".")
             whole = whole[0]
-            print(f"whole: {whole}")
+            # print(f"whole: {whole}")
             print(f"= {whole}_{modular}/{denominator}") # works
         elif int(numerator) < int(denominator):
             print(f"= {numerator}/{denominator}")
@@ -58,18 +58,18 @@ def get_num1_and_num2():
         sum = num1 - num2
         sum = str(sum)
         sum = sum.split("/")
-        print(sum)
+        # print(sum)
         numerator = sum[0]
         denominator = sum[1]
-        print(numerator, denominator)
+        # print(numerator, denominator)
         modular = int(numerator) % int(denominator)
-        print(f"modular: {modular}")
+        # print(f"modular: {modular}")
         if abs(int(numerator)) > abs(int(denominator)):
             whole = int(numerator) / int(denominator)
             whole = str(whole)
             whole = whole.split(".")
             whole = whole[0]
-            print(f"whole: {whole}")
+            # print(f"whole: {whole}")
             print(f"= {whole}_{modular}/{denominator}") # works
         elif int(numerator) < int(denominator):
             print(f"= {numerator}/{denominator}")
@@ -78,18 +78,18 @@ def get_num1_and_num2():
         sum = num1 * num2
         sum = str(sum)
         sum = sum.split("/")
-        print(sum)
+        # print(sum)
         numerator = sum[0]
         denominator = sum[1]
-        print(numerator, denominator)
+        # print(numerator, denominator)
         modular = int(numerator) % int(denominator)
-        print(f"modular: {modular}")
+        # print(f"modular: {modular}")
         if abs(int(numerator)) > abs(int(denominator)):
             whole = int(numerator) / int(denominator)
             whole = str(whole)
             whole = whole.split(".")
             whole = whole[0]
-            print(f"whole: {whole}")
+            # print(f"whole: {whole}")
             print(f"= {whole}_{modular}/{denominator}") # works    
         elif int(numerator) < int(denominator):
             print(f"= {numerator}/{denominator}")
@@ -98,18 +98,18 @@ def get_num1_and_num2():
         sum = num1 / num2
         sum = str(sum)
         sum = sum.split("/")
-        print(sum)
+        # print(sum)
         numerator = sum[0]
         denominator = sum[1]
-        print(numerator, denominator)
+        # print(numerator, denominator)
         modular = int(numerator) % int(denominator)
-        print(f"modular: {modular}")
+        # print(f"modular: {modular}")
         if int(numerator) > int(denominator):
             whole = int(numerator) / int(denominator)
             whole = str(whole)
             whole = whole.split(".")
             whole = whole[0]
-            print(f"whole: {whole}")
+            # print(f"whole: {whole}")
             print(f"= {whole}_{modular}/{denominator}") # works    
         elif int(numerator) < int(denominator):
             print(f"= {numerator}/{denominator}")
@@ -119,13 +119,13 @@ def split(word):
 
 def get_fract1(fract1):
     fract1 = split(fract1)
-    print(f"fract1: {fract1}") # works
+    # print(f"fract1: {fract1}") # works
     for i,x in enumerate(fract1):
         if fract1[i] == "/":
             fraction1 = fract1[i-1] + "/" + fract1[i+1]
-            print(f"fraction1: {fraction1}") # works
+            # print(f"fraction1: {fraction1}") # works
             num1_fraction.append(fraction1)
-            print(f"num1 fraction: {num1_fraction}") # works
+            # print(f"num1 fraction: {num1_fraction}") # works
 
 def get_fract2(fract2):
     fract2 = split(fract2)
@@ -139,35 +139,29 @@ def get_fract2(fract2):
 
 def get_num1(num1):
     num1 = split(num1)
-    print(f"num1: {num1}")
+    # print(f"num1: {num1}")
     for i,x in enumerate(num1):
         if num1[i] == "_":
             whole_number = num1[i-1]
-            print(f"whole number: {whole_number}")
+            # print(f"whole number: {whole_number}")
             num1_whole_number.append(whole_number)
-            print(f"whole_num1: {num1_whole_number}")
+            # print(f"whole_num1: {num1_whole_number}")
         elif num1[i] != "_" and num1[i]!= "/":
             num1_whole_number.append("0")
-            print(f"whole_num1: {num1_whole_number}")
-        # else:
-        #     num1_whole_number.append(sys.argv[4])
-        #     print(f"whole_num2: {num2_whole_number}")
+            # print(f"whole_num1: {num1_whole_number}")
 
 def get_num2(num2):
     num2 = split(num2)
-    print(f"num1: {num2}")
+    # print(f"num1: {num2}")
     for i,x in enumerate(num2):
         if num2[i] == "_":
             whole_number = num2[i-1]
-            print(f"whole number: {whole_number}")
+            # print(f"whole number: {whole_number}")
             num2_whole_number.append(whole_number)
-            print(f"whole_num2: {num2_whole_number}")
+            # print(f"whole_num2: {num2_whole_number}")
         elif num2[i] != "_" and num2[i]!= "/":
             num2_whole_number.append("0")
-            print(f"whole_num2: {num2_whole_number}")
-        # else:
-        #     num2_whole_number.append(sys.argv[4])
-        #     print(f"whole_num2: {num2_whole_number}")
+            # print(f"whole_num2: {num2_whole_number}")
 
 if __name__ == "__main__":
     get_num1(sys.argv[2])
